0a18fc97d20e7cfa3faa9c45ab367c4142b47336
Eugeeun <jack603@naver.com>
2024-08-15 01:22:11 +0900

    feat: Add slider to category bar

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let swiper = new Swiper('.header__category-list', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  freeMode: true,
+  grabCursor: true,
+  freeModeSticky: false,
+  freeModeMomentum: false,
+});
