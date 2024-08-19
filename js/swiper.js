e260a96708681f7cb7678d790072b93272fdf4bf
Eugeeun <jack603@naver.com>
2024-08-19 16:22:54 +0900

    refactor: Split swiper.js

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+// Swiper 초기화
+export const swiper = new Swiper('.header__category-list', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  freeMode: true,
+  grabCursor: true,
+  freeModeSticky: false,
+  freeModeMomentum: false,
+});
